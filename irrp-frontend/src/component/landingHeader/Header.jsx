0fb09fdbbe7bc0da22222c3e2ee726477225feaf
Shweta Rawat <shwetarawat524@gmail.com>
2024-04-14 12:08:09 +0530

    Changed logo size, margin in navabr, header format

--- irrp-frontend/src/component/landingHeader/Header.jsx
+++ irrp-frontend/src/component/landingHeader/Header.jsx
@@ -7,16 +7,21 @@ import IGDTUW from '../images/IGDTUW.jpeg';
 function Header() {
     return (
         <header className="Header">
-            <div className="igdtuw-container">
-                <img src={IGDTUW} alt="IGDTUW" className="igdtuw" />
-            </div>
             <div className="header-content">
-                <h1>
+                {/* <h1>
                     <span>Indira Gandhi Delhi</span>  <br />Technical <br /> University for <br /> Women
+                </h1> */}
+                <h1>
+                    <span >Indira Gandhi Delhi</span>  <br />Technical <br /> <span class="greenText">University</span> for <br /> <span class="greenText">Women</span>
                 </h1>
+
                 <p>Established by Govt. of Delhi vide Act 9 of 2012<br />ISO 9001:2015 Certified University</p>
                 <button className="login-button">Login</button>
             </div>
+            <div className="igdtuw-container">
+                <img src={IGDTUW} alt="IGDTUW" className="igdtuw" />
+            </div>
+
         </header>
     );
 }
--- irrp-frontend/src/pages/common/LoginPage.js
+++ irrp-frontend/src/pages/common/LoginPage.js
@@ -1,7 +1,9 @@
+import React from 'react';
+import Navbar from '../../component/landingNavbar/Navbar';
 function LoginPage() {
     return (
       <div className="LoginPage">
-        LoginPage
+        <Navbar />
       </div>
     );
   }
